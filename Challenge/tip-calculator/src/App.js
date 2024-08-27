69aae87c67c894ce2c90b2b5413b0b160deb8114
Sivaram V <sivaram3710@gmail.com>
2024-08-27 15:33:57 +0530

    final version

--- Challenge/tip-calculator/src/App.js
+++ Challenge/tip-calculator/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [amount, setAmount] = useState("");
+  const [yourTip, setYourTip] = useState("0");
+  const [othersTip, setOthersTip] = useState("0");
+
+  const tip = amount * ((yourTip + othersTip) / 2 / 100);
+
+  function handleReset() {
+    setAmount("");
+    setYourTip("0");
+    setOthersTip("0");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bill amount={amount} setAmount={setAmount} />
+
+      <Tip tip={yourTip} setTip={setYourTip}>
+        How did you like the service?
+      </Tip>
+
+      <Tip tip={othersTip} setTip={setOthersTip}>
+        How did your friend like the service?
+      </Tip>
+
+      {amount && (
+        <>
+          <Total amount={amount} tip={tip} />
+
+          <Reset amount={amount} onReset={handleReset} />
+        </>
+      )}
     </div>
   );
 }
 
-export default App;
+function Bill({ amount, setAmount }) {
+  return (
+    <p>
+      How much was the bill?
+      <input
+        type="text"
+        placeholder="Bill Amount"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+    </p>
+  );
+}
+
+function Tip({ tip, setTip, children }) {
+  return (
+    <p>
+      {children}
+      <select value={tip} onChange={(e) => setTip(Number(e.target.value))}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing (20%)</option>
+      </select>
+    </p>
+  );
+}
+
+function Total({ amount, tip }) {
+  return (
+    <h2>
+      You Pay ${amount + tip} (${amount} + ${tip} tip)
+    </h2>
+  );
+}
+
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
+}
