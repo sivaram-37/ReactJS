408c4c02660268608f6c827731dc1f8fafb4eaee
sivaram <sivaram3710@gmail.com>
2024-08-27 12:05:02 +0530

    v1

--- Excercise/accordion-component/src/App.js
+++ Excercise/accordion-component/src/App.js
@@ -1,3 +1,50 @@
+import { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
 export default function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <Accordion faqs={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ faqs }) {
+  return (
+    <div className="accordion">
+      {faqs.map((faq, ind) => (
+        <AccordionItem num={ind + 1} title={faq.title} text={faq.text} key={ind} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => setIsOpen((isOpen) => !isOpen)}
+    >
+      <p className="number">{num <= 9 ? `0${num}` : num}</p>
+      <p className="title">{title}</p>
+      <p className="text">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
 }
