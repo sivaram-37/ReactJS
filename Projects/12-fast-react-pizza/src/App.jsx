5823a042d8e9414f3b0120392cdee2d4e9ed4810
sivaram-37 <sivaram3710@gmail.com>
2024-09-24 13:45:08 +0530

    Implement react router in App

--- Projects/12-fast-react-pizza/src/App.jsx
+++ Projects/12-fast-react-pizza/src/App.jsx
@@ -1,5 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import OrderItem from "./features/order/OrderItem";
+
+const router = createBrowserRouter([
+	{
+		element: <AppLayout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/menu",
+				element: <Menu />,
+			},
+			{
+				path: "/cart",
+				element: <Cart />,
+			},
+			{
+				path: "/order/new",
+				element: <CreateOrder />,
+			},
+			{
+				path: "/order/:orderId",
+				element: <OrderItem />,
+			},
+		],
+	},
+]);
+
 function App() {
-	return <div>Hello Vite</div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
