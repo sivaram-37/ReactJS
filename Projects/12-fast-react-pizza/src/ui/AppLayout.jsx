de4a2277556f392e016eccec9cf10282189ecd1c
sivaram-37 <sivaram3710@gmail.com>
2024-09-24 16:07:06 +0530

    created a new component

--- Projects/12-fast-react-pizza/src/ui/AppLayout.jsx
+++ Projects/12-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
+import Loader from "./Loader";
 import CartOverview from "../features/cart/CartOverview";
 
 function AppLayout() {
+	const navigation = useNavigation();
+	const isLoading = navigation.status === "loading";
+
 	return (
-		<div>
+		<div className="layout">
+			{isLoading && <Loader />}
+
 			<Header />
 			<main>
 				<Outlet />
