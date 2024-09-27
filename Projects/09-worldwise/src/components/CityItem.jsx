8515061a322cd204f3b05d78d71f7a6833fde9d4
sivaram-37 <sivaram3710@gmail.com>
2024-09-27 16:59:01 +0530

    coverted the countrycode emoji to flag png

--- Projects/09-worldwise/src/components/CityItem.jsx
+++ Projects/09-worldwise/src/components/CityItem.jsx
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 import { useCities } from "../Contexts/CitiesProvider";
 
+const flagemojiToPNG = (flag) => {
+	var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+		.map((char) => String.fromCharCode(char - 127397).toLowerCase())
+		.join("");
+	return <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />;
+};
+
 const formatDate = (date) =>
 	new Intl.DateTimeFormat("en", {
 		day: "numeric",
@@ -28,7 +35,7 @@ function CityItem({ city }) {
 					id === currentCity.id ? styles["cityItem--active"] : ""
 				}`}
 			>
-				<span className={styles.emoji}>{emoji}</span>
+				<span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
 				<h3 className={styles.name}>{cityName}</h3>
 				<time className={styles.date}>({formatDate(date)})</time>
 				<button className={styles.deleteBtn} onClick={handleDelete}>
